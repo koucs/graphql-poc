e1d479de59a28442aff8c35fe1f974ab27fc0459
koucs <kou.cshu@gmail.com>
2020-02-08 20:07:15 +0900

    added implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,28 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds
  */
+plugins {
+    val springBootVersion = "2.2.0.RELEASE"
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("java")
+    application
+}
+
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClassName = "com.graphqljava.tutorial.bookdetails.Application"
+}
+
+dependencies {
+    implementation("com.graphql-java:graphql-java:14.0") // NEW
+    implementation("com.graphql-java:graphql-java-spring-boot-starter-webmvc:1.0") // NEW
+    implementation("com.google.guava:guava:26.0-jre") // NEW
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+repositories {
+    jcenter()
+}
